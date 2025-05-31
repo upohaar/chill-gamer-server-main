459ab323d0d28d03bd7e79bf64df5f171f161496
Upohaar <sumaiyanobeupohaar@gmail.com>
2025-05-31 21:04:17 +0600

    change index

--- index.js
+++ index.js
@@ -26,7 +26,6 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-   
     const database = client.db("GamerBD");
     const gameCollection= database.collection("gamer");
 
